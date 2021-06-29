15bf20e4a62b0af5124e52d36954d41679410185
Stefan <stefanhuynh98@gmail.com>
2021-06-29 17:24:23 +0200

    use mkdir with -p flag for it to work even if the entire tree is not available

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,12 +7,12 @@ echo "Installing packages..."
 apt install i3wm neovim tmux git curl fzf
 
 echo "Symlinking config files..."
-[ ! -d "~/.config/nvim" ] && mkdir ~/.config/nvim
+[ ! -d "~/.config/nvim" ] && mkdir -p ~/.config/nvim
 ln -sf ../config/init.vim ~/.config/nvim/init.vim
 ln -sf ../config/tmux.conf ~/.tmux.conf
 
 echo "Symlinking vim plugins..."
-[ ! -d "~/.local/share/site/start" ] && mkdir ~/.local/share/site/start
-[ ! -d "~/.local/share/site/opt" ] && mkdir ~/.local/share/site/opt
+[ ! -d "~/.local/share/site/start" ] && mkdir -p ~/.local/share/site/start
+[ ! -d "~/.local/share/site/opt" ] && mkdir -p ~/.local/share/site/opt
 ln -sf ../nvim-plugins/start ~/.local/share/nvim/start
 ln -sf ../nvim-plugins/opt ~/.local/share/nvim/opt
