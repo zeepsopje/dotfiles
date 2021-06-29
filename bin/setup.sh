5e28488ea70f3ea4ae030c5caf318215d05bd47e
Stefan <stefanhuynh98@gmail.com>
2021-06-29 17:18:37 +0200

    Update setup script: symlink config files & vim plugins

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,3 +7,12 @@ echo "Installing packages..."
 apt install i3wm neovim tmux git curl fzf
 
 echo "Symlinking config files..."
+[ ! -d "~/.config/nvim" ] && mkdir ~/.config/nvim
+ln -sf ../config/init.vim ~/.config/nvim/init.vim
+ln -sf ../config/tmux.conf ~/.tmux.conf
+
+echo "Symlinking vim plugins..."
+[ ! -d "~/.local/share/site/start" ] && mkdir ~/.local/share/site/start
+[ ! -d "~/.local/share/site/opt" ] && mkdir ~/.local/share/site/opt
+ln -sf ../nvim-plugins/start ~/.local/share/nvim/start
+ln -sf ../nvim-plugins/opt ~/.local/share/nvim/opt
