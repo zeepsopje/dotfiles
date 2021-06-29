b2c86f311ff308fe31590ab40b4e3b17375ac169
Stefan <stefanhuynh98@gmail.com>
2021-06-29 21:34:29 +0200

    fix symlinking

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,10 +3,16 @@
 # Ask for permissions up-front.
 sudo -v
 
-NVIM_PLUGIN_DIR="$HOME/share/nvim/site/pack/plugin"
+NVIM_PLUGIN_DIR="$HOME/.local/share/nvim/site/pack/plugin"
+
+echo "Updating repositories..."
+# NodeJS
+sudo curl https://deb.nodesource.com/setup_14.x | bash
+sudo apt update
 
 echo "Installing packages..."
-sudo apt install i3 neovim tmux git curl fzf
+PACKAGES=$(cat ../packages | awk '{print}' ORS=' ')
+sudo apt install $PACKAGES
 
 echo "Symlinking config files..."
 [ ! -d "~/.config/nvim" ] && mkdir -p ~/.config/nvim
@@ -14,8 +20,9 @@ ln -sf ../config/init.vim ~/.config/nvim/init.vim
 ln -sf ../config/tmux.conf ~/.tmux.conf
 
 echo "Symlinking neovim plugins..."
-[ ! -d "$NVIM_PLUGIN_DIR/start" ] && mkdir -p $NVIM_PLUGIN_DIR/start
-[ ! -d "$NVIM_PLUGIN_DIR/opt" ] && mkdir -p $NVIM_PLUGIN_DIR/opt
-ln -sf $(pwd)/nvim-plugins/start $NVIM_PLUGIN_DIR/start
-ln -sf $(pwd)/nvim-plugins/opt $NVIM_PLUGIN_DIR/opt
+echo "Checking if $NVIM_PLUGIN_DIR/[start/opt] exists"
+[[ ! -d $NVIM_PLUGIN_DIR ]] && mkdir -p $NVIM_PLUGIN_DIR
+[[ ! -d $NVIM_PLUGIN_DIR ]] && mkdir -p $NVIM_PLUGIN_DIR
+ln -sf $(pwd)/../nvim-plugins/start $NVIM_PLUGIN_DIR/
+ln -sf $(pwd)/../nvim-plugins/opt $NVIM_PLUGIN_DIR/
 
